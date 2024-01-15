806232a43a3dcc377751dde9db9a44d26a30d4fe
rklueh <r.klueh@outlook.com>
2024-01-15 14:15:37 +0100

    add intrige.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:collection/collection.dart';
 import 'package:dominion/data/game_card.dart';
 import 'package:flutter/material.dart';
 
@@ -11,16 +12,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<GameCard> _cards = [];
+  Map<String, List<GameCard>> _cards = {};
 
   List<GameCard> _basicCards = [];
+  List<GameCard> _intrigeCards = [];
 
-  void _incrementCounter() {
-    List<GameCard> _selection = [..._basicCards];
-    _selection.shuffle();
+  void _selectCards() {
+    List<GameCard> selection = [..._basicCards, ..._intrigeCards];
+    selection.shuffle();
 
     setState(() {
-      _cards = _selection.take(10).toList();
+      _cards = Map.fromEntries(selection
+          .take(10)
+          .fold<Map<String, List<GameCard>>>({}, (previousValue, element) {
+            Map<String, List<GameCard>> packages = previousValue;
+            String package = element.package;
+            if (!packages.containsKey(package)) {
+              packages[package] = [];
+            }
+            packages[package]?.add(element);
+            return packages;
+          })
+          .entries
+          .toList()
+        ..sort((a, b) => a.key.compareTo(b.key)));
     });
   }
 
@@ -29,9 +44,18 @@ class _HomePageState extends State<HomePage> {
         .loadString("data/basic.json")
         .then((value) => jsonDecode(value) as List<dynamic>)
         .then(
-            (value) => value.map((e) => GameCard(e['name'], "basic")).toList())
+            (value) => value.map((e) => GameCard(e['name'], "Basis")).toList())
         .then((value) => setState(() {
               _basicCards = value;
+            }));
+
+    DefaultAssetBundle.of(context)
+        .loadString("data/intrige.json")
+        .then((value) => jsonDecode(value) as List<dynamic>)
+        .then((value) =>
+            value.map((e) => GameCard(e['name'], "Intrige")).toList())
+        .then((value) => setState(() {
+              _intrigeCards = value;
             }));
   }
 
@@ -42,18 +66,34 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ..._cards.map((e) => Text(
-                  e.name,
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ))
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ..._cards.entries.toList().slices(3).map(
+                  (e) => Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      ...e.map(
+                        (e) => Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              e.key,
+                              style: Theme.of(context).textTheme.headlineMedium,
+                            ),
+                            ...e.value.map((card) => Text(card.name))
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: _selectCards,
+        tooltip: 'SelectCards',
         child: const Icon(Icons.refresh),
       ),
     );
